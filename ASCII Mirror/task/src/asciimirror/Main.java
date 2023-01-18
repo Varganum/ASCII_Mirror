c4e63e651a916aa6d68f8c9d5aff29bb34b5fd5b
Varganum <nikolai.sobolev@gmail.com>
2023-01-18 22:34:33 +0600

    Stage 4

--- ASCII Mirror/task/src/asciimirror/Main.java
+++ ASCII Mirror/task/src/asciimirror/Main.java
@@ -2,6 +2,7 @@ package asciimirror;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -13,11 +14,32 @@ public class Main {
 
     public static void main(String[] args) {
 
+        ArrayList<StringBuilder> linesOfFile = new ArrayList<>();
+        int maxLineLength = 0;
+        StringBuilder currentString;
+
         System.out.println(MESSAGES[0]);
 
         try (Scanner fileScanner = new Scanner(new File((new Scanner(System.in).nextLine())))) {
+
             while (fileScanner.hasNext()) {
-                System.out.println(fileScanner.nextLine());
+                currentString = new StringBuilder(fileScanner.nextLine());
+                linesOfFile.add(currentString);
+                if (currentString.length() > maxLineLength) {
+                    maxLineLength = currentString.length();
+                }
+            }
+
+            char[] spaces = new char[maxLineLength];
+            for (int i = 0; i < spaces.length; i++) {
+                spaces[i] = ' ';
+            }
+
+            for (StringBuilder line : linesOfFile) {
+                if (line.length() < maxLineLength) {
+                    line.insert(line.length(), spaces, 0, maxLineLength - line.length());
+                }
+                System.out.println(line + " | " + line);
             }
         } catch (FileNotFoundException e) {
             System.out.println(MESSAGES[1]);
